59b8385d7618635b4967e90e0c2abde6b63c069f
toby941 <toby941@gmail.com>
2014-09-03 00:34:00 +0800

    Initial Commit

---
+++ cbt/cbt/cbt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  cbt
+//
+//  Created by toby on 14-9-3.
+//  Copyright (c) 2014年 adtworker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
